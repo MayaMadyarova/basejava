4688bad4642692af64e3ba304b9872418efd5841
Maya Madyarova <maiya_mb4@mail.ru>
2024-02-15 08:05:52 +0300

    first commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,49 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int countResumes;
 
     void clear() {
+        Arrays.fill(storage, 0, countResumes, null);
+        countResumes = 0;
     }
 
     void save(Resume r) {
+        storage[countResumes] = r;
+        countResumes++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < countResumes; i++) {
+            if (storage[i].getUuid() == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < countResumes; i++) {
+            if (storage[i].getUuid() == uuid) {
+                System.arraycopy(storage, i + 1, storage, i, countResumes - i - 1);
+                storage[countResumes - 1] = null;
+                countResumes--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, countResumes);
     }
 
     int size() {
-        return 0;
+        return countResumes;
     }
 }
